5346824edf4b115196753a54b03a1cf2646d0641
UJJAL BANIYA <ujjalbaniya@gmail.com>
2020-09-05 11:53:14 +0530

    changed details to dictionary

--- myHomie.py
+++ myHomie.py
@@ -20,35 +20,45 @@ def get_ids(d):
                 yield from get_ids(v)
 
 def get_details(url):
+    url = url + '/viewform'
+    givenData = {
+        'name':'Ujjal Baniya',
+        'email':'ujjalbaniya@gmail.com',
+        'phone': '9391341962',
+        'sec':'D',
+        'address':'Nepal',
+        'comment': 'No comments',
+        'reg': '18B1A05Q3'
+    }
     url = url +"/viewform"
     html_data = requests.get(url).text
     data = json.loads( re.search(r'FB_PUBLIC_LOAD_DATA_ = (.*?);', html_data, flags=re.S).group(1) )
 
     cont = BeautifulSoup(html_data,"lxml")
     vals = cont.find_all('div', {'class':'freebirdFormviewerComponentsQuestionBaseTitle exportItemTitle freebirdCustomFont'})
-    values = [vals[i].text for i in range(len(vals))]
+    values = [vals[i].text.rstrip(' *') for i in range(len(vals))]
 
     details = {}
     for (x,y) in zip(values, get_ids(data)):
-        if y == None:
-            continue
-        else:
-            if "NAME"in x.upper():
-                details['entry.'+str(y)] = "Ujjal Baniya"
-            elif "EMAIL"in x.upper():
-                details['entry.'+str(y)] = "UjjalBaniya@gmail.com"
-            elif "PHONE"in x.upper() or "CELL" in x.upper() or "CONTACT" in x.upper():
-                details['entry.'+str(y)] = "984932362"
-            elif "REG"in x.upper():
-                details['entry.'+str(y)] = "18B91A05Q3"
-            elif "SEC"in x.upper():
-                details['entry.'+str(y)] = "D"
-            elif "BATCH"in x.upper():
-                details['entry.'+str(y)] = "2018"
-            elif "ADDRESS"in x.upper():
-                details['entry.'+str(y)] = "NEPAL"
-            elif "COMMENT" in x.upper():
-                details['entry.'+str(y)] = "No COMMENTS"
+        if type(y)==int:
+            if x.lower() in givenData.keys():
+                details['entry.'+str(y)] = givenData[x.lower()]
+#             if "NAME"in x.upper():
+#                 details['entry.'+str(y)] = "Ujjal Baniya"
+#             elif "EMAIL"in x.upper():
+#                 details['entry.'+str(y)] = "UjjalBaniya@gmail.com"
+#             elif "PHONE"in x.upper() or "CELL" in x.upper() or "CONTACT" in x.upper():
+#                 details['entry.'+str(y)] = "984932362"
+#             elif "REG"in x.upper():
+#                 details['entry.'+str(y)] = "18B91A05Q3"
+#             elif "SEC"in x.upper():
+#                 details['entry.'+str(y)] = "D"
+#             elif "BATCH"in x.upper():
+#                 details['entry.'+str(y)] = "2018"
+#             elif "ADDRESS"in x.upper():
+#                 details['entry.'+str(y)] = "NEPAL"
+#             elif "COMMENT" in x.upper():
+#                 details['entry.'+str(y)] = "No COMMENTS"
     return details
 
 
@@ -114,7 +124,7 @@ while True:
                 message = str(item["message"]["text"])
             except:
                 message = None
-                        try:
+            try:
                 fro = item["message"]["from"]["id"]
             except:
                 fro = item['edited_message']["from"]["id"]
